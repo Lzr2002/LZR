23484a1ae5e15b7398ff4f2f69de5e4a88e7fcaf
Lzr2002 <114667455+Lzr2002@users.noreply.github.com>
2022-10-12 15:45:09 +0800

    列表

--- test/app/src/main/java/com/example/test/Fragment2.java
+++ test/app/src/main/java/com/example/test/Fragment2.java
@@ -1,12 +1,22 @@
 package com.example.test;
 
 import android.os.Bundle;
+import android.content.Context;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 
 public class Fragment2 extends Fragment {
@@ -18,6 +28,22 @@ public class Fragment2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_2, container, false);
+
+        View view;
+        view=inflater.inflate(R.layout.fragment_2,container,false);
+        RecyclerView recyclerView=view.findViewById(R.id.rec);
+        List<String> list=new ArrayList<>();
+        for(int i=1;i<10;i++)
+        {
+            list.add("这是第"+i+" 个列表");
+        }
+
+        Context context=getContext();
+        Myadapter adapter =new Myadapter(context,list);
+        LinearLayoutManager layoutManager =new LinearLayoutManager(context);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+        return view;
     }
 }
---
+++ test/app/src/main/java/com/example/test/Myadapter.java
@@ -0,0 +1,58 @@
+package com.example.test;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Myadapter extends RecyclerView.Adapter<Myadapter.Myholder> {
+
+    private View view;
+    private Context context;
+    private List<String> list;
+
+    public Myadapter(Context context,List<String> list) {
+        this.context = context;
+        this.list=list;
+    }
+
+    public Myadapter() {
+    }
+
+    @NonNull
+    @Override
+    public Myholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        view= LayoutInflater.from(context).inflate(R.layout.item,parent,false);
+        Myholder holder;
+        holder = new Myholder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Myholder holder, int position) {
+        holder.textView.setText(list.get(position));
+
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+
+    }
+    public class Myholder extends RecyclerView.ViewHolder{
+        private final TextView textView;
+        public Myholder(@NonNull View itemView) {
+            super(itemView);
+            textView=itemView.findViewById(R.id.textView_item);
+        }
+    }
+}
+
